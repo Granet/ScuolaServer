5627006dbf5a6a2dfca5a36db45e0d812bb57633
Granet <granet@inventati.org>
2016-01-26 09:33:44 +0100

    Update FTPuser.sh

--- FTPuser.sh
+++ FTPuser.sh
@@ -1,4 +1,11 @@
 !#/bin/bash
+
+# Controllo root
+if [ $USER != 'root' ]; then
+echo "[/!\]Tu sei root? NO. Riprova."
+exit
+fi
+# Configurazione credenziali database
 user=root
 password=toor
 database=proftpd
@@ -7,6 +14,6 @@ while read LINE
 		do
 	
 		mysql --user="$user" --password="$password" --database="$database" --execute="INSERT INTO ftpuser (userid, passwd, uid, gid, homedir, shell, count, accessed, modified) VALUES 
-(’$LINE’, ENCRYPT(’$LINE’), 2001, 2001, ‘/var/www/$LINE’, ‘/sbin/nologin’, 0, ”, ”);"
+("$LINE", ENCRYPT("$LINE"), 2001, 2001, "/var/www/$LINE", "/sbin/nologin", 0, "", "");"
   
   done < users.txt
